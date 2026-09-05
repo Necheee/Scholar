af59a1265fa977b43823f18893c892bc0ccd728a
Andaline <andalineimekaeze@gmail.com>
2026-09-05 03:04:15 +0100

    feat: add student dashboard and related routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,14 +5,19 @@ import './styles/design-tokens.css'
 import { AuthProvider } from './hooks/useAuth.jsx'
 import ProtectedRoute from './components/ProtectedRoute.jsx'
 import AppShell from './app/AppShell.jsx'
-import StudentRoutes from './app/StudentRoutes.jsx'
 import LandingPage from './pages/LandingPage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
 import RegisterPage from './pages/RegisterPage.jsx'
 import ForgotPasswordPage from './pages/ForgotPasswordPage.jsx'
 import ResetPasswordPage from './pages/ResetPasswordPage.jsx'
+import StudentDashboard from './pages/student/StudentDashboard.jsx'
+import StudentSponsorships from './pages/student/StudentSponsorships.jsx'
+import StudentApplicationPage from './pages/student/StudentApplicationPage.jsx'
+import StudentApplicationHistory from './pages/student/StudentApplicationHistory.jsx'
+import StudentProfile from './pages/student/StudentProfile.jsx'
+import StudentNotifications from './pages/student/StudentNotifications.jsx'
 
-function App() {
+export function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
@@ -25,7 +30,17 @@ function App() {
           <Route path="/reset-password" element={<ResetPasswordPage />} />
           
           <Route path="/student" element={<ProtectedRoute requiredRole="student"><AppShell role="student" /></ProtectedRoute>}>
-            <Route path="*" element={<StudentRoutes />} />
+            <Route index element={<StudentDashboard />} />
+            <Route path="sponsorships" element={<StudentSponsorships />} />
+            <Route path="sponsorships/:id" element={<StudentApplicationPage />} />
+            <Route path="application" element={<StudentApplicationPage />} />
+            <Route path="application/new" element={<StudentApplicationPage />} />
+            <Route path="application/:appId" element={<StudentApplicationPage />} />
+            <Route path="application/draft/:appId" element={<StudentApplicationPage />} />
+            <Route path="history" element={<StudentApplicationHistory />} />
+            <Route path="profile" element={<StudentProfile />} />
+            <Route path="notifications" element={<StudentNotifications />} />
+            <Route path="*" element={<Navigate to="/student" replace />} />
           </Route>
           
           <Route path="/sponsor/*" element={<ProtectedRoute requiredRole="sponsor"><AppShell role="sponsor" /></ProtectedRoute>} />
